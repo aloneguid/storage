1811c8a1a639817d160358a2514c0a691a4b9fde
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2019-01-22 11:15:33 +0000

    lower retry policy

--- src/Storage.Net.Microsoft.Azure.DataLake.Store/Blob/DirectoryBrowser.cs
+++ src/Storage.Net.Microsoft.Azure.DataLake.Store/Blob/DirectoryBrowser.cs
@@ -110,7 +110,7 @@ namespace Storage.Net.Microsoft.Azure.DataLake.Store.Blob
 
          var resp = new OperationResponse();
          List<DirectoryEntry> page = await Core.ListStatusAsync(path, listAfter, listBefore, maxEntries, userIdFormat, _client,
-            new RequestOptions(new ExponentialRetryPolicy()),
+            new RequestOptions(new ExponentialRetryPolicy(2, 1000)),
             resp);
          return page;
          //return new FileStatusOutput(listBefore, listAfter, maxEntries, userIdFormat, _client, path);
