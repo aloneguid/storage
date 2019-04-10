2b6cfab4d57330305f9cf68cd6ec9ceed5ab37ce
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2019-04-10 17:53:27 +0100

    test logging substantially improved

---
+++ test/Storage.Net.Tests.Integration/Trio/MessagingFixture.cs
@@ -0,0 +1,162 @@
+﻿using Storage.Net.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Config.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Reflection;
+
+namespace Storage.Net.Tests.Integration.Messaging
+{
+   public abstract class MessagingFixture : IDisposable
+   {
+      private const string TagPropertyName = "tag";
+      private static readonly TimeSpan MaxWaitTime = TimeSpan.FromMinutes(1);
+      private static readonly ITestSettings _settings;
+      public readonly IMessagePublisher Publisher;
+      public readonly IMessageReceiver Receiver;
+      private readonly ConcurrentDictionary<string, QueueMessage> _receivedMessages = new ConcurrentDictionary<string, QueueMessage>();
+      private QueueMessage _lastReceivedMessage;
+      private int _receivedCount;
+
+      private bool _pumpStarted = false;
+      private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+      protected readonly string _testDir;
+
+      static MessagingFixture()
+      {
+         _settings = new ConfigurationBuilder<ITestSettings>()
+            .UseIniFile("c:\\tmp\\integration-tests.ini")
+            .UseEnvironmentVariables()
+            .Build();
+      }
+
+      public MessagingFixture()
+      {
+         string buildDir = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath).Directory.FullName;
+         _testDir = Path.Combine(buildDir, "TEST-" + Guid.NewGuid());
+         Directory.CreateDirectory(_testDir);
+
+         Publisher = CreatePublisher(_settings);
+         Receiver = CreateReceiver(_settings);
+      }
+
+      protected abstract IMessagePublisher CreatePublisher(ITestSettings settings);
+
+      protected abstract IMessageReceiver CreateReceiver(ITestSettings settings);
+
+      public async Task StartPumpAsync()
+      {
+         if(_pumpStarted || Receiver == null)
+            return;
+
+         _pumpStarted = true;
+
+         //start the pump
+         await Receiver.StartMessagePumpAsync(ReceiverPumpAsync, cancellationToken: _cts.Token, maxBatchSize: 500).ConfigureAwait(false);
+      }
+
+      public async Task<string> PutMessageAsync(QueueMessage message)
+      {
+         string tag = Guid.NewGuid().ToString();
+         message.Properties[TagPropertyName] = tag;
+
+         Log("submitting tag {0}", tag);
+
+         try
+         {
+            await Publisher.PutMessagesAsync(new[] { message }).ConfigureAwait(false);
+         }
+         catch(Exception ex)
+         {
+            Log(ex.ToString());
+         }
+
+         return tag;
+      }
+
+      public async Task<QueueMessage> WaitMessageAsync(string tag, TimeSpan? maxWaitTime = null, int minCount = 1)
+      {
+         DateTime start = DateTime.UtcNow;
+
+         while((DateTime.UtcNow - start) < (maxWaitTime ?? MaxWaitTime))
+         {
+            QueueMessage candidate = GetTaggedMessage(tag);
+
+            if(candidate != null && GetMessageCount() >= minCount)
+            {
+               return candidate;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+         }
+
+         return null;
+      }
+
+
+      private void Log(string format, params object[] parameters)
+      {
+         Debug.WriteLine(string.Format("DBG: " + format, parameters));
+         Console.WriteLine("CON: " + format, parameters);
+      }
+
+      private async Task ReceiverPumpAsync(IReadOnlyCollection<QueueMessage> messages)
+      {
+         foreach(QueueMessage qm in messages)
+         {
+            qm.Properties.TryGetValue(TagPropertyName, out string tag);
+
+            if(tag != null)
+            {
+               Log("received tag: {0}", tag);
+
+               _receivedMessages.TryAdd(tag, qm);
+            }
+
+            _lastReceivedMessage = qm;
+            Interlocked.Increment(ref _receivedCount);
+         }
+
+         try
+         {
+            await Receiver.ConfirmMessagesAsync(messages);
+         }
+         catch(NotSupportedException)
+         {
+            //some provides may not support this
+         }
+         catch(Exception ex)
+         {
+            Log(ex.ToString());
+         }
+      }
+
+      public QueueMessage GetTaggedMessage(string tag)
+      {
+         if(tag == null)
+            return _lastReceivedMessage;
+
+         if(!_receivedMessages.TryGetValue(tag, out QueueMessage result))
+            return null;
+
+         return result;
+      }
+
+      public int GetMessageCount() => _receivedCount;
+
+      public void Dispose()
+      {
+         _cts.Cancel();
+
+         if(Publisher != null)
+            Publisher.Dispose();
+
+         if(Receiver != null)
+            Receiver.Dispose();
+      }
+   }
+}
--- test/Storage.Net.Tests.Integration/Trio/MessagingTest.cs
+++ test/Storage.Net.Tests.Integration/Trio/MessagingTest.cs
@@ -1,125 +1,16 @@
 ﻿using Xunit;
 using Storage.Net.Messaging;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
-using Config.Net;
 using NetBox.Generator;
-using System.Threading;
 using System.Diagnostics;
-using System.Collections.Concurrent;
-using System.IO;
-using System.Reflection;
 
 namespace Storage.Net.Tests.Integration.Messaging
 {
-   public abstract class MessagingFixture : IDisposable
-   {
-      private static readonly ITestSettings _settings;
-      public readonly IMessagePublisher Publisher;
-      public readonly IMessageReceiver Receiver;
-      private readonly ConcurrentDictionary<string, QueueMessage> _receivedMessages = new ConcurrentDictionary<string, QueueMessage>();
-      private QueueMessage _lastReceivedMessage;
-      private int _receivedCount;
-
-      private bool _pumpStarted = false;
-      private readonly CancellationTokenSource _cts = new CancellationTokenSource();
-      protected readonly string _testDir;
-
-      static MessagingFixture()
-      {
-         _settings = new ConfigurationBuilder<ITestSettings>()
-            .UseIniFile("c:\\tmp\\integration-tests.ini")
-            .UseEnvironmentVariables()
-            .Build();
-      }
-
-      public MessagingFixture()
-      {
-         string buildDir = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath).Directory.FullName;
-         _testDir = Path.Combine(buildDir, "TEST-" + Guid.NewGuid());
-         Directory.CreateDirectory(_testDir);
-
-         Publisher = CreatePublisher(_settings);
-         Receiver = CreateReceiver(_settings);
-      }
-
-      protected abstract IMessagePublisher CreatePublisher(ITestSettings settings);
-
-      protected abstract IMessageReceiver CreateReceiver(ITestSettings settings);
-
-      public async Task StartPumpAsync()
-      {
-         if(_pumpStarted || Receiver == null)
-            return;
-
-         _pumpStarted = true;
-
-         //start the pump
-         await Receiver.StartMessagePumpAsync(ReceiverPumpAsync, cancellationToken: _cts.Token, maxBatchSize: 500);
-      }
-
-      private async Task ReceiverPumpAsync(IReadOnlyCollection<QueueMessage> messages)
-      {
-         foreach(QueueMessage qm in messages)
-         {
-            string tag = qm.Properties.ContainsKey("tag")
-               ? qm.Properties["tag"]
-               : null;
-
-            if(tag != null)
-            {
-               Debug.WriteLine("received tag: " + tag);
-               Console.WriteLine("received tag: " + tag);
-
-               _receivedMessages.TryAdd(tag, qm);
-            }
-
-            _lastReceivedMessage = qm;
-            Interlocked.Increment(ref _receivedCount);
-         }
-
-         try
-         {
-            await Receiver.ConfirmMessagesAsync(messages);
-         }
-         catch(NotSupportedException)
-         {
-            //some provides may not support this
-         }
-      }
-
-      public QueueMessage GetTaggedMessage(string tag)
-      {
-         if(tag == null)
-            return _lastReceivedMessage;
-
-         if(!_receivedMessages.TryGetValue(tag, out QueueMessage result))
-            return null;
-
-         return result;
-      }
-
-      public int GetMessageCount() => _receivedCount;
-
-      public void Dispose()
-      {
-         _cts.Cancel();
-
-         if(Publisher != null)
-            Publisher.Dispose();
-
-         if(Receiver != null)
-            Receiver.Dispose();
-      }
-   }
 
    public abstract class MessagingTest : IAsyncLifetime
    {
-      private static readonly TimeSpan MaxWaitTime = TimeSpan.FromMinutes(1);
-
-
       private readonly MessagingFixture _fixture;
 
       public MessagingTest(MessagingFixture fixture)
@@ -133,37 +24,6 @@ namespace Storage.Net.Tests.Integration.Messaging
       }
 
       public Task DisposeAsync() => Task.CompletedTask;
-
-      private async Task<string> PutMessageAsync(QueueMessage message)
-      {
-         string tag = Guid.NewGuid().ToString();
-         message.Properties["tag"] = tag;
-         Console.WriteLine("submitting tag " + tag);
-         Debug.WriteLine("submitting tag " + tag);
-
-         await _fixture.Publisher.PutMessagesAsync(new[] { message });
-
-         return tag;
-      }
-
-      private async Task<QueueMessage> WaitMessage(string tag, TimeSpan? maxWaitTime = null, int minCount = 1)
-      {
-         DateTime start = DateTime.UtcNow;
-
-         while((DateTime.UtcNow - start) < (maxWaitTime ?? MaxWaitTime))
-         {
-            QueueMessage candidate = _fixture.GetTaggedMessage(tag);
-
-            if(candidate != null && _fixture.GetMessageCount() >= minCount)
-            {
-               return candidate;
-            }
-
-            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
-         }
-
-         return null;
-      }
 
       [Fact]
       public async Task SendMessage_OneMessage_DoesntCrash()
@@ -210,9 +70,9 @@ namespace Storage.Net.Tests.Integration.Messaging
       {
          string content = RandomGenerator.RandomString;
 
-         string tag = await PutMessageAsync(new QueueMessage(content));
+         string tag = await _fixture.PutMessageAsync(new QueueMessage(content));
 
-         QueueMessage received = await WaitMessage(tag);
+         QueueMessage received = await _fixture.WaitMessageAsync(tag);
 
          Assert.True(received != null, $"no messages received with tag {tag}, {_fixture.GetMessageCount()} received in total");
          Assert.Equal(content, received.StringContent);
@@ -226,9 +86,9 @@ namespace Storage.Net.Tests.Integration.Messaging
          var msg = new QueueMessage(content);
          msg.Properties["one"] = "v1";
 
-         string tag = await PutMessageAsync(msg);
+         string tag = await _fixture.PutMessageAsync(msg);
 
-         QueueMessage received = await WaitMessage(tag);
+         QueueMessage received = await _fixture.WaitMessageAsync(tag);
 
          Assert.True(received != null, "no message received with tag " + tag);
          Assert.Equal(content, received.StringContent);
@@ -240,9 +100,9 @@ namespace Storage.Net.Tests.Integration.Messaging
       {
          string content = RandomGenerator.RandomString;
          var msg = new QueueMessage(content);
-         string tag = await PutMessageAsync(msg);
+         string tag = await _fixture.PutMessageAsync(msg);
 
-         QueueMessage rmsg = await WaitMessage(tag);
+         QueueMessage rmsg = await _fixture.WaitMessageAsync(tag);
          Assert.NotNull(rmsg);
       }
 
@@ -255,7 +115,7 @@ namespace Storage.Net.Tests.Integration.Messaging
 
          await _fixture.Publisher.PutMessagesAsync(messages);
 
-         await WaitMessage(null, null, 10);
+         await _fixture.WaitMessageAsync(null, null, 10);
 
          Assert.True(_fixture.GetMessageCount() >= 10, _fixture.GetMessageCount().ToString());
       }
