7b80c0a79d69741a1fa2c1dd661c6949c9707262
Bryan Garcia <bryan@numindsoftware.com>
2019-12-19 00:44:52 -0500

    adding sftp support, initial library dependencies and structure

---
+++ src/Storage.Net.SFtp/ConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Storage.Net.Blobs;
+using Storage.Net.ConnectionString;
+using Storage.Net.Messaging;
+
+namespace Storage.Net.SFtp
+{
+   class ConnectionFactory : IConnectionFactory
+   {
+      public const ushort DefaultPort = 22;
+
+      public IBlobStorage CreateBlobStorage(StorageConnectionString connectionString)
+      {
+         if (connectionString.Prefix == "sftp")
+         {
+            connectionString.GetRequired("host", true, out string host);
+            connectionString.GetRequired("user", true, out string user);
+            connectionString.GetRequired("password", true, out string password);
+            ushort port = ushort.TryParse(connectionString.Get("port"), out port) ? port : DefaultPort;
+            return new SshNetSFtpBlobStorage(host, port, user, password);
+         }
+         return null;
+      }
+
+      public IMessenger CreateMessenger(StorageConnectionString connectionString)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
