9e713657a3f9602b3f99b77f9c6c075dadcfc5fc
Ivan G <ivan.gavryliuk@outlook.com>
2020-01-06 11:07:28 +0000

    try this

--- infra/postconfigure.ps1
+++ infra/postconfigure.ps1
@@ -2,12 +2,14 @@ param(
     [string] $JsonString
 )
 
-Import-Module Az.DataLakeStore
+#Import-Module Az.DataLakeStore
 
 $Json = ConvertFrom-Json $JsonString
 $Gen1AccountName = $Json.azureGen1StorageName.value
 $OperatorObjectId = $Json.operatorObjectId.value
 $TestUserObjectId = $Json.testUserObjectId.value
+
+Write-Host "setting permissions for Data Lake Gen 1 ($Gen1AccountName)..."
 
 Set-AzDataLakeStoreItemAclEntry -Account $Gen1AccountName -Path / -AceType User `
     -Id $OperatorObjectId -Permissions All -Recurse -Concurrency 128
