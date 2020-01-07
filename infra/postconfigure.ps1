20b11ace253a353b02ad45dd4587d250d7efb856
Ivan G <ivan.gavryliuk@outlook.com>
2020-01-07 11:13:17 +0000

    names

--- infra/postconfigure.ps1
+++ infra/postconfigure.ps1
@@ -12,15 +12,10 @@ $Gen2AccountName = $json.azureGen2StorageName.value
 $OperatorObjectId = $Json.operatorObjectId.value
 $TestUserObjectId = $Json.testUserObjectId.value
 
-
-
 Write-Host "setting permissions for Data Lake Gen 1 ($Gen1AccountName)..."
-
 # fails when ACL is already set
-
 Set-AzDataLakeStoreItemAclEntry -Account $Gen1AccountName -Path / -AceType User `
     -Id $OperatorObjectId -Permissions All -Recurse -Concurrency 128 -ErrorAction SilentlyContinue
-
 Set-AzDataLakeStoreItemAclEntry -Account $Gen1AccountName -Path / -AceType User `
     -Id $TestUserObjectId -Permissions All -Recurse -Concurrency 128 -ErrorAction SilentlyContinue
 
